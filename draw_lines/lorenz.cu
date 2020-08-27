2963d0ef90b40b97aadaf8ec7d6235912049b834
lee unseo <lee.unseo17@gmail.com>
2020-08-27 14:28:29 +0900

    euler method

--- draw_lines/lorenz.cu
+++ draw_lines/lorenz.cu
@@ -5,7 +5,7 @@
 #define sigma 10
 #define rho 28
 #define beta 8.0/3.0
-#define scale 30.0
+#define scale 28.0
 
 struct color {
     int red;
@@ -48,8 +48,9 @@ void line( unsigned char *ptr, color c, point_idx p1, point_idx p2) {
 }
 
 void kernel( unsigned char *ptr ) {
-    int t_max = 80;
-    float dt = 0.001;
+    float t_max = 80;
+    float dt = 0.00007;
+    float t = 0;
 
     float x = 1.0;
     float y = 1.0;
@@ -60,27 +61,33 @@ void kernel( unsigned char *ptr ) {
     int xid2, yid2;
 
     for (int i=0; i<int(t_max/dt); i++) {
+        t = t + dt;
         x = x + (sigma * (y - x))*dt;
         y = y + (x * (rho - z) - y)*dt;
         z = z + (x * y - beta * z)*dt;
-        color c = {int(255 * z / 50), 128, int(255 - 255 * z / 50), 255};
+        // color c = {128, int(255*(sin(5*t/t_max)+1)/2.0), 128, 255};
+        // color c = {int(255 * t / t_max), int(255*(sin(5*t/t_max)+1)/2.0), int(255 - 255 * t / t_max), 255};
+
+        // 04
+        color c = {int(255 * t / t_max), 0, int(255 - 255 * t / t_max), 255};
+
         xid2 = cal_ind(x);
         yid2 = cal_ind(y);
-        point_idx p1 = {xid1, yid1};
-        point_idx p2 = {xid2, yid2};
-        line(ptr, c, p1, p2);
+        // point_idx p1 = {xid1, yid1};
+        // point_idx p2 = {xid2, yid2};
+        // line(ptr, c, p1, p2);
 
         xid1 = xid2;
         yid1 = yid2;
 
-        // int xid = cal_ind(x);
-        // int yid = cal_ind(y);
-        // int offset = xid + yid * DIM;
+        int xid = cal_ind(x);
+        int yid = cal_ind(y);
+        int offset = xid + yid * DIM;
 
-        // ptr[offset*4 + 0] = 255;
-        // ptr[offset*4 + 1] = int(155 * z / 50);
-        // ptr[offset*4 + 2] = int(255 * z / 50);
-        // ptr[offset*4 + 3] = 255;
+        ptr[offset*4 + 0] = c.red;
+        ptr[offset*4 + 1] = c.yellow;
+        ptr[offset*4 + 2] = c.blue;
+        ptr[offset*4 + 3] = c._;
     }
 }
 
